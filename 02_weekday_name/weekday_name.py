410b049035086e69f1765ae9c97bb74276ad156e
TechEdDan2 <dg.nelson2@gmail.com>
2024-09-02 14:50:13 -0400

    Completed Parts 1 & 2

--- 02_weekday_name/weekday_name.py
+++ 02_weekday_name/weekday_name.py
@@ -12,3 +12,21 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    wk_name = {
+        1: "Sunday",
+        2: "Monday",
+        3: "Tuesday",
+        4: "Wednesday",
+        5: "Thursday",
+        6: "Friday",
+        7: "Saturday"
+    }
+
+    if not isinstance(day_of_week, int):
+        print('Enter a number between 1 - 7')
+        return None
+
+    if day_of_week in wk_name.keys():
+        return wk_name[day_of_week]
+    
+    return None
